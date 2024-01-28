b234bb57e8ffff2de6031815433c2f8180a0b166
Tarik Khoumri <tarik.khmr@gmail.com>
2024-01-29 00:11:03 +0100

    create the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
                 <FaMoon/>
             </Button>
             <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue' color='gray'>
+                <Button gradientDuoTone='purpleToBlue' outline color='gray'>
                     Sign In
                 </Button>
             </Link>
